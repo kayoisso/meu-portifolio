079113c5ca4f4577f487ef367320811eabb3b562
kayoisso <kayowilson219@gmail.com>
2025-06-15 01:18:00 -0300

    corrigindo bug na navbar

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const track = document.getElementById("carousel-track");
   const btnLeft = document.getElementById("carousel-left");
